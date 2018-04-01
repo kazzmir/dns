1a270e66827d741801befed17b8ce151e3b60f82
Jon Rafkind <jon@rafkind.com>
2018-04-01 15:09:15 -0700

    udp port works

--- server.rs
+++ server.rs
@@ -1,3 +1,10 @@
+use std::net::UdpSocket;
+
 fn main(){
-  println!("Hello world!");
+  let port = 5553;
+  let socket = UdpSocket::bind(format!("127.0.0.1:{port}", port=port)).unwrap();
+  println!("Running server on port {port}", port=port);
+  let mut buffer = [0; 100];
+  let (amount, source) = socket.recv_from(&mut buffer).unwrap();
+  println!("Received {bytes} {source}", bytes=amount, source=source);
 }
