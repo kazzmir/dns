2c89218d3348ad3b667db8e7a32930509345684c
Jon Rafkind <jon@rafkind.com>
2018-05-06 20:15:06 -0700

    wrap the u8 buffer in an object that can extract le16 values

--- server.rs
+++ server.rs
@@ -37,17 +37,44 @@ fn dns_zero(bits: u16) -> u8 {
     return (bits & 0b111) as u8;
 }
 
+struct ByteReader<'a>{
+    buffer: &'a [u8],
+    index: usize
+}
+
+trait Reader{
+    fn read_le_16(&mut self) -> u16;
+}
+
+impl<'a> Reader for ByteReader<'a> {
+    fn read_le_16(&mut self) -> u16 {
+        let value:u16 = u16::from_le(((self.buffer[self.index] as u16) << 8) |
+                                       self.buffer[self.index + 1] as u16);
+        self.index += 2;
+        return value;
+    }
+}
+
+fn make_byte_reader(buffer: &[u8]) -> ByteReader {
+    return ByteReader{
+        buffer: buffer,
+        index: 0
+    }
+}
+
 /* FIXME: the type of buffer used to be [u8; 12] but it seems we have to convert
  * a slice into an array to achieve that.
  */
 fn parse_dns_header(buffer: &[u8]) -> DNSHeader {
-    /* TODO: clean up repeated u16::from_le expressions */
-    let identity:u16 = u16::from_le(((buffer[0] as u16) << 8) | buffer[1] as u16);
-    let data_bits:u16 = u16::from_le(((buffer[2] as u16) << 8) | buffer[3] as u16);
-    let question_count:u16 = u16::from_le(((buffer[4] as u16) << 8) | buffer[5] as u16); 
-    let answer_record:u16 = u16::from_le(((buffer[6] as u16) << 8) | buffer[7] as u16); 
-    let authority_record_count:u16 = u16::from_le(((buffer[8] as u16) << 8) | buffer[9] as u16); 
-    let additional_record_count:u16 = u16::from_le(((buffer[10] as u16) << 8) | buffer[11] as u16); 
+    let mut reader = make_byte_reader(buffer);
+
+    let identity:u16 = reader.read_le_16();
+    let data_bits:u16 = reader.read_le_16();
+    let question_count:u16 = reader.read_le_16();
+    let answer_record:u16 = reader.read_le_16();
+    let authority_record_count:u16 = reader.read_le_16();
+    let additional_record_count:u16 = reader.read_le_16();
+
     println!("Identity is {identity}", identity=identity);
     println!("Question count: {question}", question=question_count);
     println!("Zero field: {zero}", zero=dns_zero(data_bits));
@@ -97,7 +124,7 @@ fn parse_dns_question(buffer: &[u8]) -> DNSQuestion {
 fn main(){
   let port = 5553;
   let socket = UdpSocket::bind(format!("127.0.0.1:{port}", port=port)).unwrap();
-  println!("Running server on port {port}", port=port);
+  println!("Running DNS server on port {port}", port=port);
   let mut buffer = [0; 4096];
   let (amount, source) = socket.recv_from(&mut buffer).unwrap();
   println!("Received {bytes} {source}", bytes=amount, source=source);
